6dc2cdefce329d47c7f4c06588b8345be5765462
ananta <12180395+ananta@users.noreply.github.com>
2024-01-16 21:57:35 -0500

    refactor(main): cleanup boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
-// Import the native module. On web, it will be resolved to ReactNativePedometer.web.ts
-// and on native platforms to ReactNativePedometer.ts
-import ReactNativePedometerModule from './ReactNativePedometerModule';
-import ReactNativePedometerView from './ReactNativePedometerView';
-import { ChangeEventPayload, ReactNativePedometerViewProps } from './ReactNativePedometer.types';
-
-// Get the native constant value.
-export const PI = ReactNativePedometerModule.PI;
+import ReactNativePedometerModule from "./ReactNativePedometerModule";
 
 export function hello(): string {
   return ReactNativePedometerModule.hello();
 }
-
-export async function setValueAsync(value: string) {
-  return await ReactNativePedometerModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ReactNativePedometerModule ?? NativeModulesProxy.ReactNativePedometer);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ReactNativePedometerView, ReactNativePedometerViewProps, ChangeEventPayload };
